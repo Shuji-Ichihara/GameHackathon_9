eae830dd52845ee0d88f7e1246ea769238273b51
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-02-01 17:28:45 +0900

    Delete TapNotes
    ノーツのタップ判定の処理を削除した。

--- GameHackathon_9/Assets/Users/Scripts/Notes.cs
+++ GameHackathon_9/Assets/Users/Scripts/Notes.cs
@@ -7,15 +7,6 @@ public class Notes : MonoBehaviour
     [SerializeField]
     private float _moveSpeed = 5.0f;
 
-    private bool IsTap = false;
-    private NotesManager _notesManager = null;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        _notesManager = NotesManager.Instance;
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -23,24 +14,11 @@ public class Notes : MonoBehaviour
     }
 
     /// <summary>
-    /// Notes の移動処理
+    /// ノーツの移動処理
     /// </summary>
     private void MoveNotes()
     {
         Vector3 move = Vector3.right * _moveSpeed * Time.deltaTime ;
         transform.Translate(move);
-    }
-
-    public void TapNotes()
-    {
-        // タップ成功の範囲
-        var tapSuccess = _notesManager.TapZone.transform.position.x / 2; 
-
-        if(Input.GetKeyDown(KeyCode.Space) 
-            && (transform.position.x == _notesManager.TapZone.transform.position.x - tapSuccess
-            || transform.position.x == _notesManager.TapZone.transform.position.x + tapSuccess))
-        {
-            IsTap = true;
-        }
     }
 }
