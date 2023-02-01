624627b165140d3d067ac1ee72587e4b9595cc12
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-02-01 14:33:13 +0900

    Create NotesManager
    ノーツを管理するマネージャークラスを作成した。

---
+++ GameHackathon_9/Assets/Users/Scripts/NotesManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesManager : SingletonMonoBehaviour<NotesManager>
+{
+    [SerializeField]
+    private List<Transform> _notePops = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
