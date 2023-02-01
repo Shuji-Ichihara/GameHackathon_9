c9241e0dc535886cbfcb0af73a4f95423091c221
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-02-01 15:24:32 +0900

    Create Notes
    ノーツ本体が持つ機能のスクリプトを作成した。

---
+++ GameHackathon_9/Assets/Users/Scripts/Notes.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5.0f;
+
+    private bool IsTap = false;
+    private NotesManager _notesManager = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _notesManager = NotesManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveNotes();
+    }
+
+    /// <summary>
+    /// Notes の移動処理
+    /// </summary>
+    private void MoveNotes()
+    {
+        Vector3 move = Vector3.right * _moveSpeed * Time.deltaTime ;
+        transform.Translate(move);
+    }
+
+    public void TapNotes()
+    {
+        // タップ成功の範囲
+        var tapSuccess = _notesManager.TapZone.transform.position.x / 2; 
+
+        if(Input.GetKeyDown(KeyCode.Space) 
+            && (transform.position.x == _notesManager.TapZone.transform.position.x - tapSuccess
+            || transform.position.x == _notesManager.TapZone.transform.position.x + tapSuccess))
+        {
+            IsTap = true;
+        }
+    }
+}
