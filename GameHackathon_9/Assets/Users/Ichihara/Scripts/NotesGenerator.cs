bc1347a9d5abc951db0fe3a992eabc4eb5c715b0
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2023-02-03 16:25:56 +0900

    Update NotesGenerator.cs
    _list_notes の公開領域を Public にして、外部のスクリプトから参照できるようにした。

--- GameHackathon_9/Assets/Users/Ichihara/Scripts/NotesGenerator.cs
+++ GameHackathon_9/Assets/Users/Ichihara/Scripts/NotesGenerator.cs
@@ -10,20 +10,19 @@ public class NotesGenerator : MonoBehaviour
 {
     [SerializeField]
     private GameObject _notesObject = null;
-    public GameObject NotesObject { get; set; }
     [SerializeField]
     private const int _maxNotes = 10;
 
-    private List<PoolObject> _list_notes = new List<PoolObject>();
+    [System.NonSerialized]
+    public List<PoolObject> _list_notes = new List<PoolObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        NotesObject = _notesObject;
         for (int i = 0; i < _maxNotes; i++)
         {
             PoolObject poolObject;
-            poolObject = Instantiate(NotesObject).GetComponent<PoolObject>();
+            poolObject = Instantiate(_notesObject).GetComponent<PoolObject>();
             poolObject.transform.parent = transform;
             poolObject.gameObject.SetActive(false);
             _list_notes.Add(poolObject);
